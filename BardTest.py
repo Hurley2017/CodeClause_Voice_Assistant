4e52b1f085809729028fb1750296237bf9ad298f
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-26 22:35:39 +0530

    done!

--- BardTest.py
+++ BardTest.py
@@ -1,5 +1,6 @@
 from bardapi import Bard
 import os
+import json
 import gtts
 import speech_recognition as sr
 import pyttsx3
@@ -7,13 +8,20 @@ from playsound import playsound
 from dotenv import load_dotenv
 load_dotenv()
 
+def loadWithJson():
+    file = open("settings.json")
+    replacements = json.load(file)
+    return replacements
+
+replacements = loadWithJson()
+
 def invokeaudiototext():
     driver = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Command : ")
+        print("listening....")
         inp = driver.listen(source)
         text = driver.recognize_google(inp)
-        print(text)
+        # print(text)
     return text
 
 def ttsReverb(text):
@@ -23,21 +31,21 @@ def ttsReverb(text):
     
 def playandremove(temp):
     obs = gtts.gTTS(text=temp, lang='en', slow=False)
-    obs.save('Temp.mp3')
+    obs.save(replacements["file_name"])
     audio_file = os.path.dirname(__file__) + '\Temp.mp3'
     playsound(audio_file)
-    os.remove('temp.mp3')
+    os.remove(replacements["file_name"])
     print(audio_file)
 
-print("Running...")
+print("Loading...")
 Tusher = input("Hey! What do you want me to call you?\n= ")
 print("Setting Environment.. This may take some time!\n")
 API_key = os.environ.get("API_KEY")
 ai = Bard(token=API_key)
-test = ai.get_answer("My name is "+Tusher+" remember that! And I will call you Friday from now! I expect you to answer me as Friday.")
+test = ai.get_answer(replacements["name"]+Tusher+replacements["friday"])
 while True:
-    Query = invokeaudiototext() + " reply me in not more than 100 words!"
-    if(Query == 'exit reply me in not more than 100 words!'):
+    Query = invokeaudiototext()
+    if(Query == 'exit'):
         break
     received = ai.get_answer(Query)
     parse = received['content']
